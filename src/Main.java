5bc451f22491dfa606ec9c1d31656c4ccda6772f
xueyujiangnan <chenyunet@sina.com>
2014-09-02 15:27:22 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import juyee.com.cy.frame.MusicPlayer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final MusicPlayer = new MusicPlayer();
+
+    }
+}
---
+++ src/juyee/com/cy/frame/MusicPlayer.java
@@ -0,0 +1,19 @@
+package juyee.com.cy.frame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Administrator on 2014/9/2.
+ */
+public class MusicPlayer extends JFrame implements ActionListener{
+
+    MusicPlayer(){
+        super(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
